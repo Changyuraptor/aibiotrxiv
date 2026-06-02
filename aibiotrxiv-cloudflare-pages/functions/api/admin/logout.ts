4b94b3878bc92288db426c49dad0081b2a1c17a3
Changyuraptor <changyuraptor.dinosaur@gmail.com>
2026-06-02 23:58:08 +0800

    delete

--- aibiotrxiv-cloudflare-pages/functions/api/admin/logout.ts
+++
@@ -1,5 +0,0 @@
-import { clearAdminCookie } from './_session';
-
-export async function onRequestPost() {
-  return Response.json({ ok: true }, { headers: { 'Set-Cookie': clearAdminCookie() } });
-}
