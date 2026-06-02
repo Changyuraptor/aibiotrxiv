5ca57ae5578e8192874234d02800558e45622516
Changyuraptor <changyuraptor.dinosaur@gmail.com>
2026-06-03 01:15:15 +0800

    deleted

--- aibiotrxiv-cloudflare-pages/functions/api/member/session/index.ts
+++
@@ -1,2 +0,0 @@
-import { currentMember, json } from '../../_shared';
-export async function onRequestGet({request,env}:any){ const member=await currentMember(env,request); return json({ok:true,member}); }
